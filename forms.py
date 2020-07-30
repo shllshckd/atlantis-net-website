b945f95118659d2ec9884d3a5967606bce5289a9
shllshckd <52105864+shllshckd@users.noreply.github.com>
2020-07-30 20:39:48 +0300

    Revert "commit files"
    This reverts commit d27d7817e3420639cf089f4fd56d8d38e2523dd7.

--- forms.py
+++
@@ -1,9 +0,0 @@
-from flask_wtf import Form
-from wtforms import TextField, TextAreaField, SubmitField, TextField, validators, ValidationError
-
-class ContactForm(Form):
-  name = TextField("Име",  [validators.Required("Моля, въведете вашето име.")])
-  email = TextField("E-mail адрес",  [validators.Required("Моля, въведете вашия имейл адрес."), validators.Email("Моля, въведете вашия имейл адрес.")])
-  subject = TextField("Тема",  [validators.Required("Моля, въведете тема.")])
-  message = TextAreaField("Съобщение",  [validators.Required("Моля, въведете съобщение.")])
-  submit = SubmitField("Изпрати")

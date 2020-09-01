311b7015f85ae5b979e1763d6623f2b18578e449
shllshckd <52105864+shllshckd@users.noreply.github.com>
2020-09-01 12:27:33 +0300

    design update, change Form to FlaskForm in forms.py
    design update, change Form to FlaskForm because 1st one is deprecated

--- forms.py
+++ forms.py
@@ -1,7 +1,7 @@
-from flask_wtf import Form
+from flask_wtf import FlaskForm
 from wtforms import TextField, TextAreaField, SubmitField, TextField, validators, ValidationError
 
-class ContactForm(Form):
+class ContactForm(FlaskForm):
   name = TextField("Име",  [validators.Required("Моля, въведете вашето име.")])
   email = TextField("E-mail адрес",  [validators.Required("Моля, въведете вашия имейл адрес."), validators.Email("Моля, въведете вашия имейл адрес.")])
   subject = TextField("Тема",  [validators.Required("Моля, въведете тема.")])
